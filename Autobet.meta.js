248a3bb3b5cb4753c6df0f477bb9a3b3e003db1d
E4glescre4m <hinkler.clemens@web.de>
2016-04-19 17:03:45 +0200

    Create Autobet.meta.js

---
+++ Autobet.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name            [BETA]CSGODouble AUTOBET by Eagle
+// @description     An userscript that automates csgodouble.com betting using martingale system.
+// @namespace       AUTOBET by Eagle
+// @version         0.2
+// @author          Eagle
+// @match           http://www.csgodouble.com/
+// @match           http://www.csgodouble.com/index.php
+// @match           http://csgodouble.com/
+// @match           http://csgodouble.com/index.php
+// @downloadURL     https://gist.github.com/E4glescre4m/e5b21475e8f176e33e66ac5fee1fdf5b/raw/Autobet.user.js
+// @updateURL       https://gist.github.com/E4glescre4m/e5b21475e8f176e33e66ac5fee1fdf5b/raw/Autobet.meta.js
+// @run-at          document-end
+// @grant           none
+// ==/UserScript==
