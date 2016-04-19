c9011fe23215a13270aa803dec3012b572e30670
E4glescre4m <hinkler.clemens@web.de>
2016-04-19 18:26:02 +0200

    Update Autobet.user.js

--- Autobet.user.js
+++ Autobet.user.js
@@ -8,8 +8,8 @@
 // @match           http://www.csgodouble.com/index.php
 // @match           http://csgodouble.com/
 // @match           http://csgodouble.com/index.php
-// @downloadURL     https://gist.github.com/E4glescre4m/e5b21475e8f176e33e66ac5fee1fdf5b/raw/Autobet.user.js
-// @updateURL       https://gist.github.com/E4glescre4m/e5b21475e8f176e33e66ac5fee1fdf5b/raw/Autobet.meta.js
+// @downloadURL     https://github.com/E4glescre4m/Autobet/blob/master/Autobet.user.js
+// @updateURL       https://github.com/E4glescre4m/Autobet/blob/master/Autobet.meta.js
 // @run-at          document-end
 // @grant           none
 // ==/UserScript==
