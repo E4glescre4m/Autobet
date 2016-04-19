751f9ba9018718846b253954667908a843427406
E4glescre4m <hinkler.clemens@web.de>
2016-04-19 19:03:51 +0200

    Closed beta
    closed beta started

--- Autobet.user.js
+++ Autobet.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name            [BETA] CSGODouble AUTOBET by Eagle
+// @name            [ClosedBETA] CSGODouble AUTOBET by Eagle
 // @description     An userscript for Csgodouble
 // @namespace       AUTOBET by Eagle
-// @version         1.6
+// @version         2.0
 // @author          Eagle
 // @match           http://www.csgodouble.com/
 // @match           http://www.csgodouble.com/index.php
